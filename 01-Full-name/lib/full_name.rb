e258fc6d2c5d50401726570f1c15a9e46c576c4a
test-account-for-roy <adel.muursepp@real.edu.ee>
2020-03-17 17:04:25 +0200

    text added

--- 01-Full-name/lib/full_name.rb
+++ 01-Full-name/lib/full_name.rb
@@ -1,7 +1,1 @@
-def full_name(first_name, last_name)
-  # TODO: return the full name in a nice format
-
-end
-
-# To see the result of this method you can uncomment the line below:
-# puts full_name("michael", "jackson")
+print("Hello Roy")
